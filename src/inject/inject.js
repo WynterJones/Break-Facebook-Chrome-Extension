ae94d7fd073d7c07406d29d8d446f3e50100faca
Wynter Jones <wynter@monetizedesign.com>
2020-09-24 19:10:26 -0400

    hello there

---
+++ src/inject/inject.js
@@ -0,0 +1,5 @@
+chrome.extension.sendMessage({}, function (response) {
+  alert(
+    "Stop. This is a Website intended to distract you and possibly cause you to harm others by sharing offensive me mes."
+  );
+});
